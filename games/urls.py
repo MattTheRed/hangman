bc10e910cf4e7c5eb07cba58b3606cda6c7ad66b
pindardev <pindardev@gmail.com>
2015-01-08 20:49:15 -0500

    Added basic api

--- games/urls.py
+++ games/urls.py
@@ -1,10 +1,40 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from hangman.models import GameUser as User
+from hangman.models import Game
+from rest_framework import serializers, viewsets, routers
+
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username',)
+
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Game
+        fields = ('guess_count','outcome',)
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class GameViewSet(viewsets.ModelViewSet):
+    queryset = Game.objects.all()
+    serializer_class = GameSerializer
+
+# # Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'games', GameViewSet)
 
 urlpatterns = patterns('',
-    # Examples:
     # url(r'^$', 'games.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
+
+
