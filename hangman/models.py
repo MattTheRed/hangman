b478dab131865dde66c1d8924356daa3c00f339d
pindardev <pindardev@gmail.com>
2015-01-09 11:28:55 -0500

    Added model methods to handle guessing and figuring out remaining letters

--- hangman/models.py
+++ hangman/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+import string
 import json
 
 class GameUser(User):
@@ -36,12 +37,25 @@ class Word(models.Model):
 class Game(models.Model):
     user = models.ForeignKey(GameUser)
     word = models.ForeignKey(Word)
-    guess_count = models.PositiveSmallIntegerField(default=0)
+    # guess_count = models.PositiveSmallIntegerField(default=0)
     OUTCOME_CHOICES = (
         ('WIN', 'Win'),
         ('LOSS', 'Loss')
     )
     outcome = models.CharField(max_length=4, choices=OUTCOME_CHOICES)
+
+    alphabet = list(string.ascii_uppercase)
+
+    @property
+    def guess_count(self):
+        return Guess.objects.filter(game=self).count()
+
+    @property
+    def remaining_letters(self):
+        letter_list = self.alphabet[:]
+        for letter in self.guesses:
+            letter_list.remove(letter.letter)
+        return letter_list
 
     @property
     def json(self):
@@ -51,17 +65,27 @@ class Game(models.Model):
             "losses": self.user.losses,
             "game": {
                 "guess_count": self.guess_count,
-                "outcome": self.outcome
+                "outcome": self.outcome,
+                "remaining_letters": self.remaining_letters,
+                "guesses": [guess.letter for guess in self.guesses]
             }
         }
         return json.dumps(data)
 
+    @property
+    def guesses(self):
+        return Guess.objects.filter(game=self)
+
+    def make_guess(self, letter):
+        Guess.objects.create(game=self, letter=letter)
 
     def __unicode__(self):
         return self.word.word
+
     def save(self, *args, **kwargs):
         self.word = Word.objects.all().order_by("?")[0]
         super(Game, self).save(*args, **kwargs)
 
-# class Guess(models.Model):
-#     game =
+class Guess(models.Model):
+    game = models.ForeignKey(Game)
+    letter = models.CharField(max_length=1)
